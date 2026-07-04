4e6c26154574c9614f073e4c8453e57742d09465
Obin Sturm <54367380+obin1@users.noreply.github.com>
2026-07-03 22:59:44 -0600

    two reactions! PAN OH loss and Ox sink

--- jpm-mech.jl
+++ jpm-mech.jl
@@ -33,8 +33,17 @@ function termolecular(k₀298,n, k∞298, m, tempk,press)
     return kf 
 end
 
+function bimolecular_uncertainty(f298,g,tempk)
+    # calculate the uncertainty in the bimolecular rate constant
+    # f298 is the rate constant at 298 K
+    # g is additional uncertainty arising from temperature extrapolation
+    # tempk is the temperature in K
+    f = f298 * exp( abs( g * (1/tempk - 1/298.0) ) )
+    return f
+end
 
-function newmodel(press=1.0::Float64, tempk=298.0::Float64)
+
+function julia_photochem(press=1.0::Float64, tempk=298.0::Float64)
     # function definition to convert from cm3 molec-1 s-1 to (ppm min)-1
 	# 7.34e15 is Avogadro's number divided by R in L atm K-1 mol-1 divided by 1000 to convert to cm3 from L and 1e6 to convert to (ppm)-1
 	# These units are valid for bimolecular reactions
@@ -55,11 +64,13 @@ function newmodel(press=1.0::Float64, tempk=298.0::Float64)
     # this is a photolysis reaction with rate constant sampled from the GEOS-CF
     # chemical state at the surface of Los Angeles, using the KPP Standalone interface
     rk1(hv) = 9.258186912960969E-003 * hv
+    f1 = 0.0
 
     # Reaction 2 is O₃ + NO → NO₂ + O₂
     # updated July 2024, JPL 19-5 page 84 Table 1C 
     # original was rkppmmin(2.e-12 * exp(-1400/tempk), press, tempk)
     rk2(tempk) = arrhenius(3.0e-12,1500,tempk) 
+    f2 = bimolecular_uncertainty(1.07,130.0,tempk)
 
     # Reaction 3 is HCHO + O₂ → HO₂ + CO
     # photolysis reaction, rate constant taken from original mechanism
@@ -67,6 +78,7 @@ function newmodel(press=1.0::Float64, tempk=298.0::Float64)
     # this is a photolysis reaction with rate constant sampled from the GEOS-CF
     # chemical state at the surface of Los Angeles, using the KPP Standalone interface
     rk3(hv) = 3.442262702424394E-005 * hv
+    f3 = 0.0
 
     # Reaction 4 is HCHO → H₂ + CO
     # photolysis reaction, rate constant from original mechanism
@@ -74,6 +86,7 @@ function newmodel(press=1.0::Float64, tempk=298.0::Float64)
     # this is a photolysis reaction with rate constant sampled from the GEOS-CF
     # chemical state at the surface of Los Angeles, using the KPP Standalone interface
     rk4(hv) = 5.132233465281091E-005 * hv
+    f4 = 0.0
 
 
     # Reaction 5 is HCHO + OH + O₂ → HO₂ + CO + H₂O
@@ -82,11 +95,14 @@ function newmodel(press=1.0::Float64, tempk=298.0::Float64)
     # (formyl radical in oxygen makes HO₂ and CO, GEOS-Chem also parameterizes this as instantaneous)
     # original rate was rkppmmin(1.2e-14 * tempk^1 * exp(+287/tempk), press, tempk)
     rk5(tempk) = arrhenius(5.5e-12,-125.0,tempk)
+    f5 = bimolecular_uncertainty(1.15,50.0,tempk)
+
 
     # Reaction 6 is HO₂ + NO → OH + NO₂
     # updated July 2024, JPL 19-5 page 83 Table 1C
     # original was rk7(press, tempk) = rkppmmin(3.7e-12 * exp(+250/tempk), press, tempk)
     rk6(tempk) = arrhenius(3.44e-12,-260,tempk)
+    f6 = bimolecular_uncertainty(1.10,0.0,tempk) # yes that value for g of 0.0 was doublechecked with JPL 19-5 page 83 Table 1C
 
     # Reaction 7 is OH + NO₂ → HNO₃
     # updated July 2024, JPL 19-5 page 434 Table 2-1
@@ -100,6 +116,7 @@ function newmodel(press=1.0::Float64, tempk=298.0::Float64)
     #     rk8 = rkppmmin(rk8,press,tempk)
     # end
     rk7(tempk,press) = termolecular(1.8e-30,3.0,2.8e-11,0,tempk,press)
+    f7 = bimolecular_uncertainty(1.3,100.0,tempk) 
 
     # Reaction 8 is H₂O₂ → 2HO
     # this is a photolysis reaction, rate constant from original mechanism below
@@ -107,43 +124,65 @@ function newmodel(press=1.0::Float64, tempk=298.0::Float64)
     # this is a photolysis reaction with rate constant sampled from the GEOS-CF
     # chemical state at the surface of Los Angeles, using the KPP Standalone interface
     rk8(hv) = 7.373314014460711E-006 * hv
+    f8 = 0.0
 
     # Reaction 9 is H₂O₂ + OH → H₂O + HO₂
     # updated July 2024, JPL 19-5 page 73
     # "the recommendation is a temperature independent value of 1.8e-12 cm3 molec-1 s-1"
     # original was rkppmmin(3.3e-12 * exp(-200.0/tempk),press,tempk)
     rk9() = 1.8e-12
+    f9 = bimolecular_uncertainty(1.15,45.0,tempk) 
 
     # Reaction 10 is ALD2 + OH → MCO₃ + H₂O
     # updated July 2024, JPL 19-5 page 107 Table 1D
     # follows an Arrhenius equation with A = 4.63e-12, E/R = -350
     # note that GCARR_ac has the negative in it already so 
     # this is equivalent to GCARR_ac(4.63d-12, 350.0d0);
-    rk10(tempk) = arrhenius(4.63e-12,-350,tempk)
+    rk10(tempk) = arrhenius(4.63e-12,-350.0,tempk)
+    f10 = bimolecular_uncertainty(1.05,20.0,tempk) 
 
     # Reaction 11 is MGLY + hv → MCO₃ + CO + HO₂ 
     # this is a photolysis reaction with rate constant sampled from the GEOS-CF
     # chemical state at the surface of Los Angeles, using the KPP Standalone interface
     rk11(hv) = 1.863860764162095E-004 * hv 
+    f11 = 0.0
 
 
     # Reaction 12 is MCO₃ + NO₂ {+M} → PAN
     # updated July 2024, JPL 19-5 page 435 Table 2-1
-    rk12(tempk,press) = termolecular(7.3e-29,4.1,9.5e-12,1.6,tempk,press) 
+    # MCO3 is peroxyacetyl radical CH3C(O)O2 in GEOS-Chem secret code
+    rk12(tempk,press) = termolecular(7.3e-29,4.1,9.5e-12,1.6,tempk,press)
+    f12 = bimolecular_uncertainty(1.05,0.0,tempk) # yes that value for g of 0.0 was doublechecked with JPL 19-5 page 435 Table 2-1 
 
     # Reaction 13 is thermal decomposition of PAN → MCO₃ + NO₂ 
     # updated July 2024, JPL 19-5 page 521 Table 3-1
     # using the equilibrium constant divided by the same termolecular function as reaction 12
     # to get the backward rate constant
     rk13(tempk,press) = termolecular(7.3e-29,4.1,9.5e-12,1.6,tempk,press) / ( 9.0e-29*exp(14000/tempk) )
+    f13 = f12 * bimolecular_uncertainty(1.2,2000.0,tempk) 
 
+    # Reaction 14 is PAN + OH → HCHO + CO + NO₂ + H₂O
+    # updated July 2026, from GEOS-Chem v14.7.0
+    # 3.00d-14 is less than the JPL 19-5 recommendation <4.0d-14 in reaction D46
+    rk14() = 3.0e-14
+    f14 = 0.0
+
+    # Reaction 15 is O3 + OH → HO2 + O2
+    # updated July 2026, JPL 19-5 page 67 table 1B reaction B6
+    rk15(tempk) = arrhenius(1.7e-12,940.0,tempk)
+    f15 = bimolecular_uncertainty(1.15,50.0,tempk)
 
 
 
     # Evaluate the rate constant functions to get their values
     p = [rk1(hv), rk2(tempk), rk3(hv), rk4(hv), rk5(tempk),
         rk6(tempk), rk7(press,tempk), rk8(hv), rk9(), rk10(tempk), 
-        rk11(hv), rk12(tempk,press), rk13(tempk,press)]
+        rk11(hv), rk12(tempk,press), rk13(tempk,press), rk14(), rk15(tempk)]
+    f_uncertainty = [
+        f1, f2, f3, f4, f5,
+        f6, f7, f8, f9, f10,
+        f11, f12, f13, f14, f15
+    ]
 
 
     rn = @reaction_network begin
@@ -156,10 +195,12 @@ function newmodel(press=1.0::Float64, tempk=298.0::Float64)
         rk7, OH + NO₂ → HNO₃
         rk8, H₂O₂ → 2OH
         rk9, H₂O₂ + OH → H₂O + HO₂
-        rk10/O₂, ALD2 + OH + O₂ → MCO₃ + H₂O
-        rk11/(O₂^2), MGLY + 2O₂ → MCO₃ + CO + HO₂
+        rk10/O₂, ALD2 + OH + O₂ → MCO₃ + H₂O  # rate law not proportional to O₂
+        rk11/(O₂^2), MGLY + 2O₂ → MCO₃ + CO + HO₂  # rate law not proportional to O₂
         rk12, MCO₃ + NO₂ → PAN
         rk13, PAN → MCO₃ + NO₂
+        rk14, PAN + OH → HCHO + CO + NO₂ + H₂O
+        rk15, O₃ + OH → HO₂ + O₂
         # All species go to null for testing
         # rk2*1e12, NO₂ → ∅;
         # rk5*1e12, NO → ∅;
@@ -177,7 +218,7 @@ function newmodel(press=1.0::Float64, tempk=298.0::Float64)
         # rk13*1e12, PAN → ∅;
         # rk13*1e12, ALD2 → ∅;
         # rk13*1e12, MGLY → ∅;
-    end rk1 rk2 rk3 rk4 rk5 rk6 rk7 rk8 rk9 rk10 rk11 rk12 rk13
+    end rk1 rk2 rk3 rk4 rk5 rk6 rk7 rk8 rk9 rk10 rk11 rk12 rk13 rk14 rk15
 
     nspecs = size(species(rn),1)
 
@@ -197,14 +238,14 @@ function newmodel(press=1.0::Float64, tempk=298.0::Float64)
     atoms[specmap["HNO₃"], :]   = [0 1 1 3]
     atoms[specmap["CO"],   :]   = [1 0 0 1]
     atoms[specmap["H₂"],   :]   = [0 0 2 0]
-    atoms[specmap["H₂O"],  :]   = [0 0 2 1]
-    atoms[specmap["O₂"],   :]   = [0 0 0 2]
     atoms[specmap["ALD2"], :]   = [2 0 4 1]
     atoms[specmap["MGLY"], :]   = [3 0 4 2]
     atoms[specmap["MCO₃"], :]   = [2 0 3 3]
     atoms[specmap["PAN"],  :]   = [2 1 3 5]
+    atoms[specmap["H₂O"],  :]   = [0 0 2 1]
+    atoms[specmap["O₂"],   :]   = [0 0 0 2]
 
-    return rn, atoms, specmap, p
+    return rn, atoms, specmap, p, f_uncertainty
 end
 
 struct ChemSys
@@ -212,6 +253,7 @@ struct ChemSys
     atoms::Array{Float64, 2}
     specmap::Dict
     p::Array{Float64, 1}
+    f_uncertainty::Array{Float64, 1}
 end
 
 
@@ -225,7 +267,7 @@ function molec_cm3_to_ppb(molec_cm3::Float64, tempk::Float64, press::Float64)
 end
 
 
-ChemSys(press=1.0::Real, tempk=298.0::Real) = ChemSys(newmodel(press, tempk)...)
+ChemSys(press=1.0::Real, tempk=298.0::Real) = ChemSys(julia_photochem(press, tempk)...)
 jpm = ChemSys()
 graph = Graph(jpm.rn)
 # save the graph to a file
@@ -236,7 +278,7 @@ function plot_trajectories(jpm, tempk, press, vars_to_plot=["O₃", "NO", "NO₂
     # Initialize c0 with zeros
     c0 = zeros(Float64, length(jpm.specmap))
     # Set specific initial conditions
-    c0[jpm.specmap["O₂"], :] .= 0.21e6  # O₂: 0.21e6 ppm
+    c0[jpm.specmap["O₂"], :] .= 0.21e9  # O₂: 0.21e9 ppb
 
     # Initialize concentrations from uniform distribution
     c0[jpm.specmap["O₃"], :]   .= 50*rand()         
@@ -262,7 +304,7 @@ function plot_trajectories(jpm, tempk, press, vars_to_plot=["O₃", "NO", "NO₂
     sol = solve(prob, Rosenbrock23(), saveat=1.0, progress=true)
 
     # Get indices of variables to plot
-    indices = [jpm.specmap[var] for var in vars_to_plot]
+    global indices = [jpm.specmap[var] for var in vars_to_plot]
     # Extract the variables at every time step
     selected_vars = [state[indices] for state in sol.u]
     # Transpose the matrix to get each variable as a separate vector
@@ -303,7 +345,7 @@ function plot_selected_vars_transposed(selected_vars_transposed, vars_to_plot, s
     display(p)
 end
 
-# plot_trajectories(jpm, tempk, press)
+plot_trajectories(jpm, tempk, press)
 
 z = rand(Float64)*2000 # up to 2 km
 function barometric(z::Float64)
@@ -319,10 +361,131 @@ end
 # does jpm.atoms * the tendencies equal zero?
 # if so, mass is conserved
 # tendencies are the difference in concentration at each time step
-# M = jpm.atoms[indices,:]
-# tendencies = diff(selected_vars_transposed, dims=1)
-# mass_balance = tendencies*M
+M = jpm.atoms[indices,:]
+tendencies = diff(selected_vars_transposed, dims=1)
+mass_balance = tendencies*M
 
+# function get_reaction_rates(jpm, concentrations)
+#     ns = length(jpm.rn.states)
+#     nr = length(jpm.rn.eqs)
+    
+#     # Unpack the reaction rate constants (if needed)
+#     rk1, rk2, rk3, rk4, rk5, rk6, rk7, rk8, rk9, rk10, rk11, rk12, rk13 = jpm.p
+
+#     # Build the symbolic reaction vector for each reaction in the network
+#     symbolicreactionvector = [oderatelaw(jpm.rn.eqs[i], combinatoric_ratelaw=false) for i in 1:nr]
+#     expr = build_function(symbolicreactionvector,
+#                           vcat([jpm.rn.states[i] for i in 1:ns],
+#                                [jpm.rn.eqs[i].rate for i in 1:nr]))
+#     # evaluate_rxn_rate = eval(expr[2])
+#     evaluate_rxn_rate = eval(expr[2])
+
+#     reaction_rates = zeros(Float64, nr)
+    
+#     # Evaluate the reaction rates given the concentrations and the parameters
+#     evaluate_rxn_rate(reaction_rates, vcat(concentrations, jpm.p))
+    
+#     return reaction_rates
+# end
+
+# function get_chemical_states(num_experiments::Int)
+# num_experiments = 1000
+#     # This runs num_experiments simulations of the JPM mechanism
+#     # with random initial conditions and returns the final chemical state of each simulation
+#     # including the reaction rates
+#     # concentrations and reaction rates in molecules/cm^3 and molecules/cm^3/s
+#     # initialize the column names vector
+#     indices = [jpm.specmap[var] for var in ["O₃", "NO", "NO₂", "HCHO", "HO₂", "H₂O₂", "OH", "HNO₃", "CO", "H₂", "ALD2", "MGLY", "MCO₃", "PAN", "H₂O", "O₂"]]
+#     ns = length(jpm.rn.states)
+#     nr = length(jpm.rn.eqs)
+#     reaction_rate_label = ["R"*string(i) for i in 1:nr]
+#     rate_constant_label = ["k"*string(i) for i in 1:nr]
+#     rate_constant_uncertainty_label = ["f"*string(i) for i in 1:nr]
+#     label = ["Experiment number", "O3", "NO", 
+#              "NO2", "HCHO", "HO2",
+#              "H2O2", "OH", "HNO3", 
+#              "CO", "H2", "ALD2", 
+#              "MGLY", "MCO3", "PAN", 
+#              "H2O", "O2",
+#              reaction_rate_label..., 
+#              rate_constant_label...,
+#              rate_constant_uncertainty_label...]
+
+#     # Initialize the experiments array
+#     experiments_array = Array{Any}(undef, (num_experiments + 1), length(label))
+#     experiments_array[1,:] = label
+
+#     # Unpack the reaction rate constants (if needed)
+#     rk1, rk2, rk3, rk4, rk5, rk6, rk7, rk8, rk9, rk10, rk11, rk12, rk13 = jpm.p
+
+#     # Loop over the number of experiments
+#     for i in 1:num_experiments
+#         println("Experiment number: ", i)
+        
+#         # Initialize c0 with zeros
+#         c0 = zeros(Float64, length(jpm.specmap))
+#         # Set specific initial conditions
+#         c0[jpm.specmap["O₂"], :] .= 0.21e9  # O₂: 0.21e9 ppb
+
+#         # Initialize concentrations from uniform distribution
+#         c0[jpm.specmap["O₃"], :]   .= 50*rand()         
+#         c0[jpm.specmap["NO"], :]   .= 20*rand()   
+#         c0[jpm.specmap["NO₂"], :]  .= 20*rand()
+#         c0[jpm.specmap["HCHO"], :] .= 50*rand()  
+#         c0[jpm.specmap["HO₂"], :]  .= 0.01*rand()
+#         c0[jpm.specmap["H₂O₂"], :] .= 10*rand()
+#         c0[jpm.specmap["OH"], :]   .= 0.001*rand() 
+#         c0[jpm.specmap["MCO₃"], :] .= 0.01*rand()
+#         c0[jpm.specmap["PAN"], :]  .= 25*rand()
+#         c0[jpm.specmap["ALD2"], :] .= 10*rand()
+#         c0[jpm.specmap["MGLY"], :] .= 50*rand()
+        
+#         # Convert concentrations to molecules/cm^3
+#         tempk, press = barometric(1.0) # assume a height of 1000 m
+#         c0 = ppb_to_molec_cm3.(c0, tempk, press)
+
+#         # run for 20 minutes
+#         tspan = (0.0, 1200.0) # 20 minutes in seconds
+#         # Create the ODE problem
+#         prob = ODEProblem(jpm.rn, c0, tspan, jpm.p, combinatoric_ratelaws = false)
+#         # Solve the ODE problem
+#         sol = solve(prob, Rosenbrock23(), saveat=1200.0, progress=false)
+        
+#         # get the final 
+#         cfinal = sol.u[end]
+
+#         # Build the symbolic reaction vector for each reaction in the network
+#         symbolicreactionvector = [oderatelaw(jpm.rn.eqs[i], combinatoric_ratelaw=false) for i in 1:nr]
+#         expr = build_function(symbolicreactionvector,
+#                             vcat([jpm.rn.states[i] for i in 1:ns],
+#                                 [jpm.rn.eqs[i].rate for i in 1:nr]))
+#         # evaluate_rxn_rate = eval(expr[2])
+#         evaluate_rxn_rate = eval(expr[2])
+
+#         reaction_rates = zeros(Float64, nr)
+
+
+        
+#         # Evaluate the reaction rates given the concentrations and the parameters
+#         evaluate_rxn_rate(reaction_rates, vcat(cfinal, jpm.p))
+#         # create the row for the experiments array
+#         experiments_array[i+1, :] = vcat([Integer(i)], 
+#                                          cfinal[indices], 
+#                                          reaction_rates, 
+#                                          jpm.p,
+#                                          jpm.f_uncertainty)
+                                        
+
+#     end
+
+# save the experiments array to a CSV file
+# CSV.write("jpm_chemical_states_moleccm3.csv", DataFrame(experiments_array[2:end, :],experiments_array[1, :]))
+    # Return the experiments array
+#     return experiments_array
+# end
+
+# get the chemical states for 2 experiments
+# chemical_states_array = get_chemical_states(num_experiments)
 
 
 # function to run N simulations of 1 hour, with random initial conditions, sampling every 5 minutes
@@ -344,7 +507,7 @@ function run_simulations(num_tests::Int, duration::Float64, sampling_interval::F
         # Initialize c0 with zeros
         c0 = zeros(Float64, length(jpm.specmap))
         # Set specific initial conditions
-        c0[jpm.specmap["O₂"], :] .= 0.21e6  # O₂: 0.21e6 ppm
+        c0[jpm.specmap["O₂"], :] .= 0.21e9  # O₂: 0.21e6 ppm
         # Initialize concentrations from uniform distribution
         c0[jpm.specmap["O₃"], :]   .= 50*rand()         
         c0[jpm.specmap["NO"], :]   .= 20*rand()   
@@ -383,13 +546,9 @@ function run_simulations(num_tests::Int, duration::Float64, sampling_interval::F
 end
 
 # number_of_experiments = Integer(1e6)
-number_of_experiments = Integer(1)
+number_of_experiments = Integer(10)
 # number_of_experiments = 3
 experiments_array = run_simulations(number_of_experiments, 3600.0, 300.0)
-# save the experiments array to a csv file
-# CSV.write("experiments_11e5_1hour_5mins_falsecombinatoricratelaws.csv", 
-#           DataFrame(experiments_array[2:end, :], 
-#           Symbol.(experiments_array[1, :])))
 
 # mass balance checker
 M = zeros(Int, 16, 4)
@@ -418,7 +577,10 @@ tendencies = tendencies_diff[1:13:end, :]
 maximum(tendencies*M, dims=1)
 
 
-
+# save the experiments array to a csv file
+# CSV.write("experiments_11e5_1hour_5mins_falsecombinatoricratelaws.csv", 
+#           DataFrame(experiments_array[2:end, :], 
+#           Symbol.(experiments_array[1, :])))
 
 # convert csv array to selected_vars_transposed
 # selected_vars_transposed = experiments_array[2:end, 3:end]
@@ -449,14 +611,17 @@ for j in 1:size(B, 2)
     end
 end
 
-println(edgelist)
+# println(edgelist)
 
 # save the edgelist to a csv file
 # with the header:
-header = ["species index (starts from 1)","from","to","stoichiometric value "]
+# header = ["species index (starts from 1)","from","to","stoichiometric value "]
 # Create the DataFrame without specifying the header
-df = DataFrame(edgelist)
+# df = DataFrame(edgelist)
 
 # Rename the columns to the desired string headers
-rename!(df, header)
+# rename!(df, header)
 # CSV.write("jpm_edgelist.csv", df)
+
+# rewrite rk1,..., as REPL-scope values from jpm.p
+
